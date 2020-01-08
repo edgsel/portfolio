8a44dd95a0b41fab0c476a04dff7185ee12d26dd
edgsel <edgar.selihov@gmail.com>
2020-01-08 22:13:05 +0200

    base parallax background in progress, seems to be working

---
+++ project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <parallaxStars></parallaxStars>
+  </div>
+</template>
+
+<script>
+  import parallaxStars  from './components/parallaxStars';
+
+  export default {
+    name: 'app',
+    components: {
+      parallaxStars
+    }
+  }
+</script>
+
+<style>
+  #app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    /*color: #2c3e50;*/
+    transition: 0.5s;
+  }
+</style>
---
+++ project/src/components/parallaxStars.vue
@@ -0,0 +1,89 @@
+<template>
+    <div id="main">
+        <div class="grid-container">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "parallaxStars",
+        methods: {
+            createStars(quantity) {
+                for (let i = 0; i < quantity; i++) {
+                    let star = document.createElement('div');
+                    star.classList.add('star', 'type-' + String(this.randomNumber(1,3)));
+                    star.style.left = String(this.randomNumber(1,99) + '%');
+                    star.style.bottom = String(this.randomNumber(1,99) + '%');
+                    star.style.animationDuration = String(this.randomNumber(50,200) + 's');
+                    document.getElementsByClassName("grid-container")[0].appendChild(star);
+                }
+            },
+
+            randomNumber(min, max) {
+                return Math.floor(Math.random() * max) + min;
+            }
+        },
+
+        mounted() {
+            this.createStars(150);
+        }
+    }
+</script>
+
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+    }
+
+    #main {
+        margin-left: auto;
+        margin-right: auto;
+        overflow-x: hidden;
+        overflow-y: hidden;
+        background: radial-gradient(ellipse at bottom, #013155 0%, #01162E 100%);
+    }
+
+    .grid-container {
+
+        display: grid;
+        position: relative;
+        grid-template-rows: 1fr;
+        width: 100vw;
+        height: 100vh;
+        /*min-height: 100vh;*/
+        align-items: start;
+    }
+
+    .star {
+        position: absolute;
+        background: white;
+        border-radius: 50px;
+        animation: move;
+        animation-iteration-count: infinite;
+        animation-timing-function: linear;
+    }
+
+    .type-1 {
+        width: 1px;
+        height: 1px;
+    }
+
+    .type-2 {
+        width: 3px;
+        height: 3px;
+    }
+
+    .type-3 {
+        width: 4px;
+        height: 4px;
+    }
+
+
+    @keyframes move {
+        0% { transform: translateY(1000px); }
+        100% {transform: translateY(-1000px); }
+    }
+
+</style>
